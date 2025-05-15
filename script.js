94d45ab3d23c75b3043a836229957530e39e9d23
Pixel-Paladin123 <saschaleejones@hotmail.co.uk>
2025-05-15 11:58:02 +0100

    make script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Test");
+
+/*const multiLineSentence = "this is a sentence\nthat is split"
+console.log(multiLineSentence);
+
+const insertedVariable = "inserted variable"
+console.log("this is a sentence with a " + insertedVariable + " in it");
+
+const variableSentence = `this is a sentence with a ${insertedVariable} in it`;
+console.log(variableSentence);
+
+let userInput =  "HeLLo mY gUy";
+
+let lowerCase = userInput.toLocaleLowerCase();
+let upperCase = userInput.toLocaleUpperCase();
+console.log(upperCase);
+let fullSentence = "This is just a sentence for yapping and other Things and yapping";
+fullSentence = fullSentence.toLocaleLowerCase();
+console.log(fullSentence.length);
+let includeThings = fullSentence.includes ("things");
+console.log(includeThings);
+
+let replaceASentence = fullSentence.replace("a sentence ", "some text " )
+*/
+const reaminder = 10 % 4;
+
+console.log(reaminder);
+
+
+
+
+
+
+
